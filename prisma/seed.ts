462f1910423a478742caecc8e08b216cd36ad3de
Joseph Durrans <joseph_durrans@outlook.com>
2023-02-28 11:09:01 +0000

    Refactored movie and genre seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,13 +4,72 @@ import fs from "fs";
 
 const prisma = new PrismaClient();
 
-const seed = async () => {
-	/*
-        Creates the movies
-        Data set will need to be cleaned up before seeding
-        Remove any overflowing lines
-	*/
+const loadMovie = async (csvrow: any) => {
+	const movieData = {
+		title: csvrow.title,
+		overview: csvrow.overview,
+		posterPath: csvrow.poster_path,
+		runtime: +csvrow.runtime,
+		budget: +csvrow.budget,
+		revenue: +csvrow.revenue,
+		releaseDate: new Date(csvrow.release_date),
+		voteAverage: +csvrow.vote_average,
+		voteCount: +csvrow.vote_count,
+	};
 
+	// Create the movies
+	await prisma.movie.upsert({
+		where: {
+			id: +csvrow.id,
+		},
+		update: movieData,
+		create: {
+			id: +csvrow.id,
+			...movieData,
+		},
+	});
+};
+
+const loadGenre = async (csvrow: any) => {
+	// Replace single quotes with double quotes to make it valid JSON
+	const genres = JSON.parse(csvrow.genres.replace(/'/g, '"'));
+
+	for (const genre of genres) {
+		// Create the genres
+		await prisma.genre.upsert({
+			where: {
+				id: +genre.id,
+			},
+			update: {
+				name: genre.name,
+			},
+			create: {
+				id: +genre.id,
+				name: genre.name,
+			},
+		});
+
+		// Create the movie-genre relationships
+		await prisma.movieGenre.upsert({
+			where: {
+				movieId_genreId: {
+					movieId: +csvrow.id,
+					genreId: +genre.id,
+				},
+			},
+			update: {},
+			create: {
+				movieId: +csvrow.id,
+				genreId: +genre.id,
+			},
+		});
+	}
+};
+
+const loadCredit = async (csvrow: any) => {};
+
+const seed = async () => {
+	// Load the movies and genres
 	fs.createReadStream("./prisma/src-data/movies_metadata.csv")
 		.pipe(
 			parse({
@@ -19,67 +78,33 @@ const seed = async () => {
 			})
 		)
 		.on("data", async csvrow => {
-			try {
-				const movieData = {
-					title: csvrow.title,
-					overview: csvrow.overview,
-					posterPath: csvrow.poster_path,
-					runtime: +csvrow.runtime,
-					budget: +csvrow.budget,
-					revenue: +csvrow.revenue,
-					releaseDate: new Date(csvrow.release_date),
-					voteAverage: +csvrow.vote_average,
-					voteCount: +csvrow.vote_count,
-				};
-
-				// Create the movies
-				await prisma.movie.upsert({
-					where: {
-						id: +csvrow.id,
-					},
-					update: movieData,
-					create: {
-						id: +csvrow.id,
-						...movieData,
-					},
-				});
-
-				// Replace single quotes with double quotes to make it valid JSON
-				const genres = JSON.parse(csvrow.genres.replace(/'/g, '"'));
-
-				for (const genre of genres) {
-					// Create the genres
-					await prisma.genre.upsert({
-						where: {
-							id: +genre.id,
-						},
-						update: {
-							name: genre.name,
-						},
-						create: {
-							id: +genre.id,
-							name: genre.name,
-						},
-					});
-
-					// Create the movie-genre relationships
-					await prisma.movieGenre.upsert({
-						where: {
-							movieId_genreId: {
-								movieId: +csvrow.id,
-								genreId: +genre.id,
-							},
-						},
-						update: {},
-						create: {
-							movieId: +csvrow.id,
-							genreId: +genre.id,
-						},
-					});
-				}
-			} catch (err) {
+			loadMovie(csvrow).catch(err => {
 				console.error(err);
-			}
+			});
+
+			loadGenre(csvrow).catch(err => {
+				console.error(err);
+			});
+		})
+		.on("end", () => {
+			console.log("done");
+		})
+		.on("error", err => {
+			console.error(err);
+		});
+
+	// Load the credits
+	fs.createReadStream("./prisma/src-data/credits.csv")
+		.pipe(
+			parse({
+				relax_column_count: true,
+				columns: true,
+			})
+		)
+		.on("data", async csvrow => {
+			loadCredit(csvrow).catch(err => {
+				console.error(err);
+			});
 		})
 		.on("end", () => {
 			console.log("done");
@@ -89,6 +114,7 @@ const seed = async () => {
 		});
 };
 
+// Run the seed
 seed()
 	.catch(err => {
 		console.error(err);
